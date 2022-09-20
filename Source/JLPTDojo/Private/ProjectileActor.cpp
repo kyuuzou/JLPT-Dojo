a454faf4485e66cc1e6eeed45abba094ab534c93
kyuuzou <newita@gmail.com>
2022-09-20 20:14:24 +0100

    Have a projectile with its own collision layer.

---
+++ Source/JLPTDojo/Private/ProjectileActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectileActor.h"
+
+// Sets default values
+AProjectileActor::AProjectileActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectileActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
