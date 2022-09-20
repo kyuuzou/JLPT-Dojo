0f9416d354fbf603c64ea54dbb9ab78dc104194c
kyuuzou <newita@gmail.com>
2022-09-20 19:55:37 +0100

    Extend GameplayStatics with templated versions of GetActorOfClass and GetAllActorsOfClass.

---
+++ Source/JLPTDojo/Public/GameplayStaticsExtended.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineUtils.h"
+#include "Kismet/GameplayStatics.h"
+
+/**
+ * 
+ */
+class JLPTDOJO_API UGameplayStaticsExtended : UGameplayStatics {
+
+public:
+	template <typename ActorClass>
+	static ActorClass* GetActorOfSubclass(UWorld* World) {
+		TActorIterator<ActorClass> It(World);
+		return *It;
+	}
+
+	template <typename ActorClass>
+	static void GetAllActorsOfSubclass(UWorld* World, TArray<ActorClass*>& Actors) {
+		Actors.Reset();
+
+		for (TActorIterator<ActorClass> It(World); It; ++It) {
+			Actors.Add(*It);
+		}
+	}
+};
