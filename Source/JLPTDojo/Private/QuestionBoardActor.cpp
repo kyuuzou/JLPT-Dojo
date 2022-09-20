204dfc8b7b8d864c3f2401ca4759b3ab2d35d935
kyuuzou <newita@gmail.com>
2022-09-20 20:20:37 +0100

    Have a board for questions.

---
+++ Source/JLPTDojo/Private/QuestionBoardActor.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QuestionBoardActor.h"
+
+#include "AnswerBoardActor.h"
+#include "Engine/DataTable.h"
+#include "FVocabularyTableRow.h"
+#include "Text3DComponent.h"
+
+AQuestionBoardActor::AQuestionBoardActor() {
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AQuestionBoardActor::BeginPlay() {
+	Super::BeginPlay();
+
+}
+
+void AQuestionBoardActor::EndPlay(const EEndPlayReason::Type EndPlayReason) {
+	Super::EndPlay(EndPlayReason);
+
+}
+
+void AQuestionBoardActor::PostInitializeComponents() {
+	Super::PostInitializeComponents();
+
+	this->MeshComponent = this->FindComponentByClass<UStaticMeshComponent>();
+	this->TextComponent = this->FindComponentByClass<UText3DComponent>();
+}
+
+void AQuestionBoardActor::SetCaption(FText Caption) {
+	this->TextComponent->SetText(Caption);
+
+	this->MeshComponent->SetMaterial(0, this->DefaultMaterial);
+}
+
+void AQuestionBoardActor::SetCorrect(bool Correct) {
+	this->MeshComponent->SetMaterial(0, Correct ? this->RightMaterial : this->WrongMaterial);
+}
+
+void AQuestionBoardActor::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/JLPTDojo/Public/QuestionBoardActor.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "QuestionBoardActor.generated.h"
+
+UCLASS()
+class JLPTDOJO_API AQuestionBoardActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AQuestionBoardActor();
+
+	virtual void PostInitializeComponents() override;
+	void SetCaption(FText Caption);
+	void SetCorrect(bool Correct);
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Data")
+	class UDataTable* DataTable;
+
+	UPROPERTY(EditAnywhere, Category = "Materials")
+	class UMaterialInterface* DefaultMaterial;
+
+	UPROPERTY(EditAnywhere, Category = "Materials")
+	class UMaterialInterface* RightMaterial;
+
+	UPROPERTY(EditAnywhere, Category = "Materials")
+	class UMaterialInterface* WrongMaterial;
+
+	class UStaticMeshComponent* MeshComponent;
+	class UText3DComponent* TextComponent;
+};
