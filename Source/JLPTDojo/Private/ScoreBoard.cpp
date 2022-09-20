ab8a714abf7a72073334e73821a49e6cb78d228c
kyuuzou <newita@gmail.com>
2022-09-20 19:52:20 +0100

    Have a score board actor for streak and correct answers.

---
+++ Source/JLPTDojo/Private/ScoreBoard.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScoreBoard.h"
+
+#include "Engine/DataTable.h"
+#include "Delegates/Delegate.h"
+#include "DojoGameState.h"
+#include "Text3DComponent.h"
+
+AScoreBoard::AScoreBoard() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AScoreBoard::BeginPlay() {
+	Super::BeginPlay();
+
+	ADojoGameState* GameState = this->GetWorld()->GetGameState<ADojoGameState>();
+	GameState->OnScoreChanged.AddUniqueDynamic(this, &AScoreBoard::OnScoreChanged);
+}
+
+void AScoreBoard::EndPlay(const EEndPlayReason::Type EndPlayReason) {
+	ADojoGameState* GameState = this->GetWorld()->GetGameState<ADojoGameState>();
+	GameState->OnScoreChanged.RemoveAll(this);
+}
+
+void AScoreBoard::OnScoreChanged(int CurrentStreak, int MaximumStreak, int RightAnswers, int TotalQuestions) {
+	FString Caption = FString::Format(*this->TextTemplate, { RightAnswers, TotalQuestions, CurrentStreak, MaximumStreak });
+	this->TextComponent->SetText(FText::FromString(Caption));
+}
+
+void AScoreBoard::PostInitializeComponents() {
+	Super::PostInitializeComponents();
+
+	this->TextComponent = this->FindComponentByClass<UText3DComponent>();
+}
+
+void AScoreBoard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/JLPTDojo/Public/ScoreBoard.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScoreBoard.generated.h"
+
+UCLASS()
+class JLPTDOJO_API AScoreBoard : public AActor {
+	GENERATED_BODY()
+	
+public:	
+	AScoreBoard();
+	
+	virtual void PostInitializeComponents() override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:	
+	UPROPERTY(EditAnywhere, meta = (MultiLine = "true"))
+	FString TextTemplate;
+
+	class UText3DComponent* TextComponent;
+
+	UFUNCTION()
+	void OnScoreChanged(int CurrentStreak, int MaximumStreak, int RightAnswers, int TotalQuestions);
+};
